c12eed2d51fe87092cd39675a3dd23876b4d3b72
van-cahall <vcahall@gmail.com>
2016-01-01 15:31:19 -0500

    every text file in ISBN_Incoming generates an Excel file in ExcelFiles\, and then is moved to ISBN_Processed"

--- lookup-book-stats-from-excel.py
+++ lookup-book-stats-from-excel.py
@@ -1,31 +1,35 @@
 # =HYPERLINK("http://www.amazon.com/gp/search/ref=sr_adv_b?search-alias=stripbooks&unfiltered=1&field-isbn="&A2&"&sort=
 from openpyxl import load_workbook
-import time, sys, webbrowser
+import time, sys, webbrowser, os
 
-	
-lines = [line.rstrip('\n') for line in open('sample-isbn-numbers.txt')]
-	
-	
-# create the new excel workbook
-xl_template = load_workbook('book-listing-template.xltm')
-ws = xl_template.active
+folder_ISBN_Incoming = "ISBN_Incoming"
+folder_ISBN_Processed= "ISBN_Processed"
+folder_ExcelFiles = "ExcelFiles"
 
-# insert the ISBN numbers
-lineNum=2
-for isbn in lines:
-	print('Processing ISBN: ' + isbn)
-	ws['A' + str(lineNum)] = isbn
-	amazon_booklist_url = "http://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-isbn=" + isbn + "&sort=relevanceexprank"
-	formula = '=HYPERLINK("' + amazon_booklist_url + '","Amazon Link")'	
-	ws['B' + str(lineNum)] = formula
-	print(formula)
-	# Open a browser tab for each book
-	webbrowser.open(amazon_booklist_url)
+# process each file in the "Incoming" folder
+curdir = os.getcwd()
+for root, dirs, files in os.walk(curdir + '\\' + folder_ISBN_Incoming, topdown=False):
+    for name in files:
+        xl_template = load_workbook('book-listing-template.xltm')
+        ws = xl_template.active
+        filename = os.path.join(root, name)
+        lines = [line.rstrip('\n') for line in open(filename)]
+        # output_filename = curdir + '\test\' + str(name) + '.xlsx'
+        # insert the ISBN numbers
+        lineNum=2
+        for isbn in lines:
+            print('Processing ISBN: ' + isbn)
+            ws['A' + str(lineNum)] = isbn
+            amazon_booklist_url = "http://www.amazon.com/gp/search/ref=sr_adv_b/?search-alias=stripbooks&unfiltered=1&field-isbn=" + isbn + "&sort=relevanceexprank"
+            formula = '=HYPERLINK("' + amazon_booklist_url + '","Amazon Link")'	
+            ws['B' + str(lineNum)] = formula
+            #print(formula)
+            # Open a browser tab for each book
+            webbrowser.open(amazon_booklist_url)
+            
+            lineNum += 1
 	
-	lineNum += 1
-
-
-
-	
-# save the new excel file	
-xl_template.save('test/book-list-' + time.strftime("%Y-%m-%d_%H-%M-%S") + '.xlsx')
+        # save the new excel file	
+        xl_template.save(folder_ExcelFiles + "\\" + name.rstrip('.txt')  + "_" + time.strftime("%Y-%m-%d_%H-%M-%S") + '.xlsx')
+        # Move the incoming text file to the processed folder
+        os.rename(filename, folder_ISBN_Processed + "\\" + name)
